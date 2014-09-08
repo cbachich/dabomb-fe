050b8b1731725bd1be96cf6849d2e97f4ef67bef
Christopher Bachicha <cbachich@gmail.com>
2014-09-08 01:14:20 -0700

    Add a very basic play/pause button

--- video-player.js
+++ video-player.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() { initializeMediaPlayer(); }, false);
+document.addEventListener("DOMContentLoaded", function() { initVideoPlayer(); }, false);
 
 var videoPlayer;
 
-function initializeMediaPlayer() {
+function initVideoPlayer() {
   videoPlayer = document.getElementById('video');
   videoPlayer.controls = false;
+}
+
+function togglePlayPause() {
+  var btn = document.getElementById('play-pause-button');
+
+  if (videoPlayer.paused || videoPlayer.ended) {
+    btn.title = 'pause';
+    btn.innerHTML = 'pause';
+    btn.className = 'pause';
+    videoPlayer.play();
+  } else {
+    btn.title = 'play';
+    btn.innerHTML = 'play';
+    btn.className = 'play';
+    videoPlayer.pause();
+  }
 }
